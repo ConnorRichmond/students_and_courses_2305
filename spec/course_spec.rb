73627b7053402823875609d1feeb49d5c5d314e5
Connor Richmond <richmond.connor@gmail.com>
2023-05-24 11:26:28 -0400

    Create test from readme

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,6 +2,23 @@ require 'rspec'
 require './lib/course'
 require './lib/student'
 
-RSpec.describe Course do 
+course = Course.new("Calculus", 2)
+student1 = Student.new({name: "Morgan", age: 21})
+student2 = Student.new({name: "Jordan", age: 29}) 
+
+describe Course do 
+
+  it 'adds students to course' do
+    expect(course.name).to eq('Calculus')
+    expect(course.capacity).to eq(2)
+    expect(course.students).to eq([])
+
+    course.enroll(student1)
+    course.enroll(student2)
+
+    expect(course.students).to eq([student1, student2])
+
+    expect(course.full?).to be true
+  end
 
 end
