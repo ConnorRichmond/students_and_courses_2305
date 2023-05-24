a54852ab2d9c79abb439bb2a9ba165e954165796
Connor Richmond <richmond.connor@gmail.com>
2023-05-24 11:16:28 -0400

    Create student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
 class Student
+
+  attr_reader :name, :age, :scores
  
+  def initialize(student_details = {})
+    @name = student_details[:name]
+    @age = student_details[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    return 0 if @scores.empty?
+
+    total_score = @scores.sum
+    average = total_score.to_f / @scores.length
+    average.round(2)
+  end
+
 end
