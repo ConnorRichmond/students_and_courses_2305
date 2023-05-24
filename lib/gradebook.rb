87bb82b3b155e55b602783692c23e396da858756
Connor Richmond <richmond.connor@gmail.com>
2023-05-24 12:17:53 -0400

    Create gradebook class from test

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,37 @@
 class Gradebook
+
+  attr_reader :instructor, :courses
+
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    students_by_course = {}
+
+    @courses.each do |course|
+      students_by_course[course] = course.students
+    end
+
+    students_by_course
+  end
+
+  #getting issue with repeating students. |= seems to have fixed that
+  # |= keeps input unique to append
+  def students_below(threshold)
+    below_threshold_students = []
   
+    @courses.each do |course|
+      below_threshold_students |= course.students.select { |student| student.grade < threshold }
+    end
+  
+    below_threshold_students
+  end
+
+
 end
