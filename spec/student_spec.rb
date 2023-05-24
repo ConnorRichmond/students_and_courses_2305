2762aefe650f258b329fd3b899cb98d3c1c33a6a
Connor Richmond <richmond.connor@gmail.com>
2023-05-24 11:16:19 -0400

    Create test for student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,6 +1,24 @@
 require 'rspec'
 require './lib/student'
 
-RSpec.describe Student do
-  
+describe Student do
+
+  it 'logs student details' do
+    student = Student.new({name: 'Morgan', age: 21})
+
+    expect(student.name).to eq('Morgan')
+    expect(student.age).to eq(21)
+  end
+
+  it 'logs student scores' do
+    student = Student.new({name: 'Morgan', age: 21})
+
+    expect(student.scores).to eq([])
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.scores).to eq([89, 78])
+
+    expect(student.grade).to eq(83.5)
+  end
+
 end
