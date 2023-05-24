17a1b1c8634278c8e95a10e76e7cce8ad54fe480
Connor Richmond <richmond.connor@gmail.com>
2023-05-24 12:42:21 -0400

    Add methods all grades and grate threshold +refac

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -21,16 +21,30 @@ class Gradebook
     students_by_course
   end
 
-  #getting issue with repeating students. |= seems to have fixed that
-  # |= keeps input unique to append
   def students_below(threshold)
     below_threshold_students = []
   
     @courses.each do |course|
-      below_threshold_students |= course.students.select { |student| student.grade < threshold }
+      below_threshold_students += course.students.select { |student| student.grade < threshold }
     end
   
     below_threshold_students
+  end
+
+  def all_grades
+    @courses.each_with_object({}) do |course, grades|
+      grades[course] = course.students.map { |student| student.grade }
+    end
+  end
+  
+  def students_in_range(min, max)
+    students = []
+    @courses.each do |course|
+      course.students.each do |student|
+        students << student if student.grade >= min && student.grade <= max
+      end
+    end
+    students
   end
 
 
