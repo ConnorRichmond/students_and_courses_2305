a100e0b1f7af7ccc72282deef44a9445b4cc7df5
Connor Richmond <richmond.connor@gmail.com>
2023-05-24 12:41:19 -0400

    Add test for all grade and grade range

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -25,9 +25,6 @@ describe Gradebook do
   end
 
   it 'returns a hash of courses and their students' do
-    gradebook.add_course(course1)
-    gradebook.add_course(course2)
-    gradebook.add_course(course3)
     course1.enroll(student1)
     course2.enroll(student2)
     course2.enroll(student3)
@@ -36,17 +33,25 @@ describe Gradebook do
   end
 
   it 'returns a list of students below the given threshold' do
-    gradebook.add_course(course1)
-    gradebook.add_course(course2)
-    course1.enroll(student1)
-    course2.enroll(student2)
-
     student1.log_score(70)
     student2.log_score(90)
     student3.log_score(45)
 
     expect(gradebook.students_below(50)).to eq([student3])
+    expect(gradebook.students_below(80)).to eq([student1, student3])
+  end
 
+  it 'returns array of grades in a course' do
+  
+    expect(gradebook.all_grades).to eq({ course1 => [70], course2 => [90, 45], course3 => [] })
+  end
+
+  it 'returns a list of students within the given grade range' do
+    expect(gradebook.students_in_range(90, 100)).to eq([student2])
+    expect(gradebook.students_in_range(80, 89)).to eq([])
+    expect(gradebook.students_in_range(70, 79)).to eq([student1])
+    expect(gradebook.students_in_range(60, 69)).to eq([])
+    expect(gradebook.students_in_range(0, 59)).to eq([student3])
   end
 
 end
